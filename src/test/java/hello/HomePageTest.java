bb0e0eaf3e6872fa67096b843c879bc4eee96c34
MartinZhao5 <40642794+MartinZhao5@users.noreply.github.com>
2019-11-17 20:39:48 -0800

    Update HomePageTest.java

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -25,6 +25,11 @@ public class HomePageTest {
 
     @Autowired
     private MockMvc mvc;
+    @MockBean
+ private AuthControllerAdvice aca;
+
+ @MockBean
+ private ClientRegistrationRepository crr;
 
     @Test
     public void getHomePage_ContentType() throws Exception {
