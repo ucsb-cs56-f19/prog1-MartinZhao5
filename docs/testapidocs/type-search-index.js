f838b37de3c5dd65ec63e60a706f36f3226e2b1a
Ziheng Zhao <ziheng@umail.ucsb.edu>
2019-12-02 22:33:18 -0800

    mz - updated javadoc and jacoco report

--- docs/testapidocs/type-search-index.js
+++ docs/testapidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"hello","l":"BootstrapLiterals"},{"p":"hello","l":"HomePageTest"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"earthquakes","l":"BootstrapLiterals"},{"p":"earthquakes","l":"HomePageTest"}]
