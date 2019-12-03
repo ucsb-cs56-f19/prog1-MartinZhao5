d03cb69ec328da38fc8a5ea5e2cb06fba883c4f2
MartinZhao5 <40642794+MartinZhao5@users.noreply.github.com>
2019-12-02 18:00:53 -0800

    mz updated controllers

--- src/main/java/hello/controllers/EarthquakeController.java
+++ src/main/java/hello/controllers/EarthquakeController.java
@@ -17,6 +17,27 @@ import earthquakes.geojson.FeatureCollection;
 
 @Controller
 public class EarthquakesController {
+  @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @GetMapping("/earthquakes/search")
+    public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            EqSearch eqSearch) {
+        return "earthquakes/search";
+    }
+
+    @GetMapping("/earthquakes/results")
+    public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            EqSearch eqSearch) {
+        EarthquakeQueryService e = new EarthquakeQueryService();
+        model.addAttribute("eqSearch", eqSearch);
+        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag(), eqSearch.getLat(), eqSearch.getLon());
+        model.addAttribute("json", json);
+        FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+        model.addAttribute("featureCollection",featureCollection);
+
+        return "earthquakes/results";
+    }
 
 }
 
