16e914a104207d9b927dbc48fe11a27d3db0af12
MartinZhao5 <40642794+MartinZhao5@users.noreply.github.com>
2019-12-04 16:17:47 -0800

    Update EqSearch.java

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -1,27 +1,53 @@
-//Plain Java Bean
 package earthquakes.searches;
 
-public class EqSearch {
+public class EqSearch{
     private int distance;
     private int minmag;
-    
-    //no-arg default constructor
+    private double lat;
+    private double lon;
+    private String location;
 
-    //Getters
+    
+    
     public int getDistance(){
-        return distance;
+	return distance;
     }
-    
+
     public int getMinmag(){
-        return minmag;
+	return minmag;
+    }
+
+    public double getLat(){
+	return lat;
+    }
+
+    public double getLon(){
+	return lon;
+    }
+
+    public String getLocation(){
+	return location;
+    }
+
+    
+    public void setDistance(int distant){
+	distance = distant;
     }
     
-    //Setters
-    public void setDistance(int d){
-        distance = d;
+    public void setMinmag(int MM){
+	minmag = MM;
     }
-    
-    public void setMinmag(int m){
-        minmag = m;
+
+    public void setLat(double Latitude){
+	lat = Latitude;
     }
+
+    public void setLon(double Longtitude){
+	lon = Longtitude;
+    }
+
+    public void setLocation(String Location){
+	location = Location;
+    }
+
 }
