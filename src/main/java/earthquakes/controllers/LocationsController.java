c511e9f06c5fcf2361c877c1500983b58cbcfdfe
MartinZhao5 <40642794+MartinZhao5@users.noreply.github.com>
2019-12-04 16:14:16 -0800

    Update LocationsController.java

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -8,40 +8,78 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
 
 import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
+import earthquakes.searches.LocSearch;
+import earthquakes.services.LocationQueryService;
 import earthquakes.osm.Place;
 
-import earthquakes.services.LocationQueryService;
-import earthquakes.searches.LocSearch;
+import earthquakes.entities.Location;
+import earthquakes.repositories.LocationRepository;
 
-import java.util.List;
+import earthquakes.geojson.FeatureCollection;
 
 
 @Controller
 public class LocationsController {
 
+    private LocationRepository locationRepository;
+
     @Autowired
     private ClientRegistrationRepository clientRegistrationRepository;
-
+    public LocationsController(LocationRepository locationRepository){
+	this.locationRepository = locationRepository;
+    }
+    
+    
     @GetMapping("/locations/search")
     public String getLocationsSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
             LocSearch locSearch) {
         return "locations/search";
     }
 
+
     @GetMapping("/locations/results")
     public String getLocationsResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
-            LocSearch locSearch) {
-        LocationQueryService e = new LocationQueryService();
-        model.addAttribute("locSearch", locSearch);
-        String json = e.getJSON(locSearch.getLocation());
-        model.addAttribute("json", json);
-        List<Place> place = Place.listFromJson(json);
-        model.addAttribute("place", place);
-        return "locations/results";
+          LocSearch locSearch) {
+
+      LocationQueryService l = new LocationQueryService();
+      model.addAttribute("locSearch", locSearch);
+      String json = l.getJSON(locSearch.getLocation());
+      model.addAttribute("json", json);
+      List<Place> place = Place.listFromJson(json);
+      model.addAttribute("place", place);
+      return "locations/results";
     }
+
+    @GetMapping("/locations")
+    public String index(Model model) {
+	
+        Iterable<Location> locations = locationRepository.findAll();
+        model.addAttribute("locations", locations);
+        return "locations/index";
+    }
+
+    @PostMapping("/locations/add")
+    public String add(Location location, Model model) {
+      locationRepository.save(location);
+      model.addAttribute("locations", locationRepository.findAll());
+      return "locations/index";
+    }
+
+    @DeleteMapping("/locations/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+	Location location = locationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+	locationRepository.delete(location);
+	model.addAttribute("locations", locationRepository.findAll());
+	return "locations/index";
+}
 }
